9db53761bc9a0503c6df749a603b16a8a47c81f9
fanidc7 <fanidc7@gmail.com>
2023-11-02 14:51:52 +0700

    tambah hapus dan ubah data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 
 import shoppingIcon from './assets/shopping-icon.svg'
+import plusIcon from './assets/plus-icon.svg'
+import minusIcon from './assets/minus-icon.svg'
 
 function App() {
   const [value, setValue] = useState('')
@@ -11,6 +13,42 @@ function App() {
     {title : 'kentang', count : 1}
   ])
 
+  const addList = (e) => {
+    e.preventDefault()
+
+    if (!value) {
+      alert('No Blank Item!')
+      return
+    }
+
+    const newTodos = [...todos, {
+      title : value, count : 1
+    }]
+
+    setTodos(newTodos)
+    setValue('')
+  }
+
+  const handleAdd = (index) => {
+    const newTodos = [...todos]
+
+    newTodos[index].count += 1
+
+    setTodos(newTodos)
+  }
+
+  const handleMin = (index) => {
+    const newTodos = [...todos]
+
+    if (newTodos[index].count > 0) {
+      newTodos[index].count -= 1
+    } else {
+      newTodos.splice(index,1)
+    }
+
+    setTodos(newTodos)
+  }
+
   return (
     <>
     <nav className='nav'>
@@ -19,7 +57,7 @@ function App() {
     </nav>
 
     <section className='container'>
-      <form className='form'>
+      <form className='form' onSubmit={addList}>
         <input className='input' type="text" placeholder='List'
           onChange={(e) => setValue(e.target.value)}
           value={value}
@@ -28,11 +66,25 @@ function App() {
       </form>
       {todos.length > 0 ? (
         <div className='todos'>
-          {todos.map((todo) => {
+          {todos.map((todo, index) => {
             return (
-              <div key={todo.title}>
+              <div key={index} className={`todo${!(todos.length == index + 1) ? ' todo-divider' : ''}`}>
                 {todo.title}
-                {todo.count}
+                
+                <div className='todo-icon-wrapper'>
+
+                <div className='todo-count'>{todo.count}</div>
+
+                <button className='todo-action-button' onClick={() => handleMin(index)}>
+                  <img src={minusIcon} alt="Minus Icon" />
+                </button>
+
+                <button className='todo-action-button' onClick={() => handleAdd(index)}>
+                  <img src={plusIcon} alt="Plus Icon" />
+                </button>
+
+                </div>
+
               </div>
             )
           })}
